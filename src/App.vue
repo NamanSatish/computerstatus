043c555b76a487f09efb18fa79fa8def2ea06c60
Naman Satish <naman.satish@gmail.com>
2020-07-03 16:29:22 -0700

    Updates

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,53 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+
+    <h3>Pivottable Ui Demo</h3>
+    <vue-pivottable
+        :data="pivotData"
+        aggregatorName='Count'
+        rendererName='Table Heatmap'
+        :rows="['computer','time']"
+        :cols="['atera']"
+        :vals="['atera']"
+    >
+    </vue-pivottable>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { VuePivottable, VuePivottableUi } from 'vue-pivottable'
+import 'vue-pivottable/dist/vue-pivottable.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    VuePivottableUi
+  },
+  data () {
+    return {
+      pivotData: [['ID', 'time', 'computer', 'atera']],
+      dataheaders: ['ID', 'time', 'computer', 'atera'],
+    }
+  },
+  mounted(){
+    axios.request({url: "http://app.relion365.com/api/computers", method:"get"} ).then((response)=>{ 
+      var data = response.data
+      var output = []
+      for(var i = 0; i< data.length ; i++){
+          var temp = []
+          var readable = new Date(data[i].created_at)
+          readable = readable.toLocaleDateString() + " " +readable.toLocaleTimeString() 
+          temp.push( data[i].id, readable, data[i].computer, data[i].atera)
+          output.push(temp);
+      }
+      output.forEach(element => {
+        this.pivotData.push(element);
+      });
+      console.log(this.pivotData)
+    })
   }
 }
 </script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VuePivottable from 'vue-pivottable'
+import 'vue-pivottable/dist/vue-pivottable.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+ 
+Vue.use(VueAxios, axios)
+Vue.use(VuePivottable)
 
 Vue.config.productionTip = false
 
